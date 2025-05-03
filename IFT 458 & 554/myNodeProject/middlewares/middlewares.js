1a62beb0b37cb94113da57a4b3cfdb01ee345518
Brandon Trinkle <btrinkle52@gmail.com>
2025-05-03 11:36:26 -0400

    more

--- IFT 458 & 554/myNodeProject/middlewares/middlewares.js
+++
@@ -1,7 +0,0 @@
-// Example middleware function to transform the request body to uppercase
-exports.makeUpperCase = (req, res, next) => {
-  if (req.body.name) {
-    req.body.name = req.body.name.toUpperCase();
-  }
-  next();
-};
